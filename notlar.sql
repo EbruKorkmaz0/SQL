73a7e956f8c69b3bc923c03a0b29434d471d6829
Ebru Korkmaz <111504729+EbruKorkmaz0@users.noreply.github.com>
2023-04-30 23:25:00 +0300

    Add files via upload

---
+++ notlar.sql
@@ -0,0 +1,16 @@
+USE [OgrenciNotSistemi]
+GO
+
+/****** Object:  Table [dbo].[Notlar]    Script Date: 30.04.2023 23:19:19 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Notlar](
+	[matematik] [int] NULL,
+	[id] [int] NOT NULL
+) ON [PRIMARY]
+GO
+
---
+++ ogrenci.sql
@@ -0,0 +1,20 @@
+USE [OgrenciNotSistemi]
+GO
+
+/****** Object:  Table [dbo].[Ogrenci]    Script Date: 30.04.2023 23:19:34 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Ogrenci](
+	[id] [int] NOT NULL,
+	[adSoyad] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_Ogrenci] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
